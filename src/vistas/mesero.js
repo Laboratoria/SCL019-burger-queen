1fae426d69e876cfae67717bbb61055e7428db08
pazvarela <89282860+pazvarela@users.noreply.github.com>
2022-05-02 11:19:47 -0400

    creacion de ruteo, boilerplate, data creada, componentes creados y vista mesero

---
+++ src/vistas/mesero.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { MenuOption } from "../components/menuOption";
+
+function VistaMenu() {
+    return (
+      <div className="App">
+       <MenuOption/>
+       
+      </div>
+      
+    );
+  }
+  
+  export default VistaMenu;
+  
